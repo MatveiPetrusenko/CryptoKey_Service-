70dae100f502a4ff2bb00e1cb7a5425d87dd3610
MatveiPetrusenko <matveipetrusenko@gmail.com>
2023-08-29 12:19:56 +0300

    created simple server & client

---
+++ server/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	pb "main.go/proto"
+)
+
+const (
+	// Port for gRPC server to listen to
+	PORT = ":50051"
+)
+
+type passwordStorageSystem struct {
+	pb.UnimplementedAuthServiceServer
+}
+
+func (s *passwordStorageSystem) RegisterUser(ctx context.Context, in *pb.User) (*pb.Result, error) {
+	log.Printf("Received: %s %s %s %s", in.GetId(), in.GetUsername(), in.GetEmail(), in.GetHashedPassword())
+
+	r := &pb.Result{
+		Success: true,
+		Message: "User " + in.GetUsername() + " registered",
+	}
+
+	return r, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", PORT)
+
+	if err != nil {
+		log.Fatalf("failed connection: %v", err)
+	}
+
+	s := grpc.NewServer()
+
+	pb.RegisterAuthServiceServer(s, &passwordStorageSystem{})
+
+	log.Printf("server listening at %v", lis.Addr())
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to server: %v", err)
+	}
+}
